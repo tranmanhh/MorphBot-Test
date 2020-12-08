9fe77f49f7d61a49e19ce13ef75d8a8a4296fde1
Manh Dinh <dinhtranmanh6181@gmail.com>
2020-12-08 10:40:20 +0700

    MorphBot Test updated

---
+++ MorphBot/Assets/Script/Cube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        transform.Translate(xInput * speed * Time.deltaTime, 0, yInput * speed * Time.deltaTime, 0);
+    }
+}
---
+++ MorphBot/Assets/Script/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            InstantiateCube();
+        }
+    }
+
+    private void InstantiateCube()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10f;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        Instantiate(cube, mousePos, transform.rotation);
+    }
+}
